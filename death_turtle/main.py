b9e9e2e532201ac43734d2000832f92548234f25
Dommert <DommertEnterprises@Gmail.com>
2014-08-06 03:38:06 -0700

    more rum
    RUM RUM RUM

--- death_turtle/main.py
+++ death_turtle/main.py
@@ -5,14 +5,21 @@ from admin import admin
 from api import api
 from models import *
 from views import *
+from flask_rum.main import rum
+import flask_rum.rum_config as rum_config
+
 
 admin.setup()
 api.setup()
 
-if __name__ == '__main__':
+app.config.from_object(rum_config)
+# Sample override of Theme
+app.config.THEME_FOLDER='rum/banana/'
 
+if __name__ == '__main__':
     auth.User.create_table(fail_silently=True)
     Note.create_table(fail_silently=True)
     Message.create_table(fail_silently=True)
     Relationship.create_table(fail_silently=True)
+    app.register_blueprint(rum)
     app.run()
